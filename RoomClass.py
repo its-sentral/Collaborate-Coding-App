7c4c83c844ea18a653ecd319b79858fd9c760a98
K-n15 <theepapon7055@gmail.com>
2026-03-15 17:28:39 +0700

    Unfinished class

---
+++ RoomClass.py
@@ -0,0 +1,65 @@
+from UserClass import Admin
+from datetime import datetime
+
+class Room(object):
+    def __init__(self, Rname, RID : int, admin:Admin, mem=[]):
+        self.roomName = Rname
+        self.roomID = RID
+        self.member = mem
+        self.admin = admin
+
+    def removeMember(self,name:str):
+        for i in self.member:
+            if i.getName() == name:
+                i.leaveRoom(self.roomID)
+                break
+
+    def removeMember(self,id:int):
+        for i in self.member:
+            if i.getName() == id:
+                i.leaveRoom(self.roomID)
+                break
+
+class Call(object):
+    def __init__(self):
+        self.startTime = datetime.now()
+        self.endTime = None
+
+    def endCall(self):
+        self.endTime = datetime.now()
+
+    def getStartTime(self):
+        return self.startTime
+    
+    def getEndTime(self):
+        return self.endTime
+    
+class Workshop(object):
+    def __init__(self):
+        pass
+
+    def importCode(self,file):
+        pass
+
+    def exportCode(self):
+        pass
+
+class Conversation(object):
+    def __init__(self,content,sender):
+        self.content = content
+        self.sender = sender
+        self.time = datetime.now()
+
+    def getDetail(self):
+        return (self.time,self.sender,self.content)
+
+class Chat(object):
+    def __init__(self):
+        self.history = []
+
+    def addConversation(self,content,sender):
+        Convo = Conversation(content,sender)
+        self.history.append(Convo)
+
+    def getChatHistory(self):
+        return self.history
